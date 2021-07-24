21ad162267b4d4ef851e29c7991deb9fd8b86e35
winnie48562000 <winnie48562000@gmail.com>
2021-07-25 02:09:23 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+
+// connect to mongodb with mongoose
+mongoose.connect('mongodb://localhost/todo-list',{ useNewUrlParser: true, useUnifiedTopology: true })
+
+// check mongoose connected
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('monogodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`App is running on localhost:${port}`)
+  console.log(`App is running on https://localhost:${port}`)
 })
