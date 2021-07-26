523d8681bd7749fd2c68989eb2e01f924175092b
winnie48562000 <winnie48562000@gmail.com>
2021-07-26 11:47:33 +0800

    feat: add update todo function

--- app.js
+++ app.js
@@ -58,6 +58,27 @@ app.get('/todos/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// Edit route
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save() // 因為儲存資料，該步驟不使用 lean()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`App is running on https://localhost:${port}`)
 })
