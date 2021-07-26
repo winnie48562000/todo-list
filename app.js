61525ff978b7430e86bd7d223a10db95ed061abe
winnie48562000 <winnie48562000@gmail.com>
2021-07-26 22:47:40 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -79,6 +79,15 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// Delete route
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`App is running on https://localhost:${port}`)
 })
