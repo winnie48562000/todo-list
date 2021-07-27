03c0109dbdea7e4f5ec538d6d131ea5afc0248a2
winnie48562000 <winnie48562000@gmail.com>
2021-07-27 10:31:10 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.get('/', (req, res) => {
   // Get all todos
   Todo.find() // 找出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JS 資料陣列
+    .sort({ _id: 'asc' }) // Mongoose排序功能：asc 正序、desc 反序
     .then(todos => res.render('index', { todos }) // transport to index
     .catch(error => console.error(error)) // 錯誤處理
   )
